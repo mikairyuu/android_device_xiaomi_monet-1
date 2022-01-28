6b06f2bfb55bfd88e464d3643fd91fa5827b3718
mikairyuu <miva200267@gmail.com>
2022-01-28 17:18:17 +0300

    Arcana bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/evolution_monet.mk
+   $(LOCAL_DIR)/aosp_monet.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_monet-user \
-    evolution_monet-userdebug \
-    evolution_monet-eng
+    aosp_monet-user \
+    aosp_monet-userdebug \
+    aosp_monet-eng
--- aosp_monet.mk
+++ aosp_monet.mk
@@ -8,28 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Inherit from monet device
 $(call inherit-product, device/xiaomi/monet/device.mk)
 
-PRODUCT_NAME := evolution_monet
+PRODUCT_NAME := aosp_monet
 PRODUCT_DEVICE := monet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10 Lite 5G
 
-EVO_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_USES_AOSP_RECOVERY := true
 
-# GrapheneOS Camera
-TARGET_BUILD_GRAPHENEOS_CAMERA = true
+ARCANA_DEVICE := monet
+ARCANA_MAINTAINER := mikairyuu
+WITH_GAPPS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
