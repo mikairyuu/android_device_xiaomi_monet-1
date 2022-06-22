24dd4d5134f4c8bcd9ebe19df08e682ca8c8277d
mikairyuu <miva200267@gmail.com>
2022-06-22 11:40:42 +1000

    monet: nuke los remains

--- device.mk
+++ device.mk
@@ -11,7 +11,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-evolution
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
